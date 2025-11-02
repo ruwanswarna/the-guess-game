22f457d7c22f9cdb3008846109be27319f123445
ruwanswarna <ruwanswarna@gmail.com>
2025-11-02 20:22:45 +0530

    practicing document.querySelector()

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent);
+console.log(document.querySelectorAll('p'));
+// console.log(document.querySelectorAll('p'));
+
+document.querySelector('.message').textContent = 'Correct number! ';
+const content = document.querySelector('.message').textContent;
+console.log(content);
+
+document.querySelector('.score').textContent = 10;
+document.querySelector('.number').textContent = 13;
+
+console.log( document.querySelector('.guess').value) 
+document.querySelector('.guess').value = 50;
