ef2e4e8dfaa2dd6c77cc80c3ea9e90097d93d4b0
ruwanswarna <ruwanswarna@gmail.com>
2025-11-02 21:36:03 +0530

    check! button works,and displays correct message when guessed too low or too high - not caring about duplicate codes for now

--- script.js
+++ script.js
@@ -16,6 +16,18 @@
 
 //eventhandler function - a helper function (javascript engine will execute this
 //function once the click event happens
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guess = Number(document.querySelector('.guess').value);
+//   console.log(guess, typeof guess);
+//   // document.querySelector('.message').textContent = 'Correct number!';
+
+//   if (!guess) {
+//     document.querySelector('.message').textContent = 'No number!';
+//   }
+// });
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -23,5 +35,23 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number! ';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Guess is too high! ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Guess is too low! ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+    }
   }
 });
